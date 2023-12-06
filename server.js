452477f723eda2c7d23b8731e0fb0b10ee349397
DamascusKraken <nick.canchola.7.27@gmail.com>
2023-12-05 23:16:13 -0600

    All files from Main Repo didnt want to deal with fork and merge problems

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
 
-// TODO: Add a comment describing the functionality of this expression
+// Add a comment describing the functionality of this expression
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({ helpers });
 
-// TODO: Add a comment describing the functionality of this object
+// Add a comment describing the functionality of this object
 const sess = {
   secret: 'Super secret secret',
   cookie: {},
@@ -27,15 +27,31 @@ const sess = {
   })
 };
 
-// TODO: Add a comment describing the functionality of this statement
+
+// Route to display static src images 
+app.get("/static", (req, res) => { 
+  res.render("static"); 
+}); 
+
+// Route to display dynamic src images 
+app.get("/dynamic", (req, res) => { 
+  imageList = []; 
+  imageList.push({ src: "Challenges/15Weekly-Challenge/Budget-Buddy/Images/2023-11-30_19-07-50.png", name: "personal budget" }); 
+  imageList.push({ src: "2023-11-30_19-09-12.png", name: "Linen" }); 
+  //imageList.push({ src: "icons/react.png", name: "react" }); 
+  res.render("dynamic", { imageList: imageList }); 
+});
+
+// Add a comment describing the functionality of this statement
 app.use(session(sess));
 
-app.engine('handlebars', hbs.engine);
+app.engine('handlebars',exphbs({ defaultLayout: "main" }));
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static("images"));
 
 app.use(routes);
 
